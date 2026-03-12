bb03c77fdcbea9a446e448e2629aae7bebd4ba60
saimeunt <saimeunt@gmail.com>
2026-03-12 02:45:00 +0100

    update

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,11 @@ const nextConfig: NextConfig = {
   rewrites: async () => ({
     beforeFiles: [
       {
+        source: "/_next/static/media/miden_client_web.e6753bbd.wasm",
+        destination:
+          "https://miden-playground-assets.walnut.dev/_next/static/media/miden_client_web.e00a052b.wasm",
+      },
+      {
         source: "/:path*",
         destination: `https://miden-playground.walnuthq.workers.dev/:path*`,
       },
