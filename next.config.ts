3924a79500bfd2bd9307166b1d884e1f1f40cc25
saimeunt <saimeunt@gmail.com>
2026-03-19 21:43:44 +0100

    update

--- next.config.ts
+++ next.config.ts
@@ -1,20 +1,24 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  redirects: async () => [
+    {
+      source: "/_next/static/media/miden_client_web.8318b253.wasm",
+      // 0.13.3
+      destination:
+        "https://miden-playground-assets.walnut.dev/_next/static/media/miden_client_web.24b3664b.wasm",
+      permanent: true,
+    },
+    {
+      source: "/_next/static/media/miden_client_web.e6753bbd.wasm",
+      // 0.13.2
+      destination:
+        "https://miden-playground-assets.walnut.dev/_next/static/media/miden_client_web.e00a052b.wasm",
+      permanent: true,
+    },
+  ],
   rewrites: async () => ({
     beforeFiles: [
-      {
-        source: "/_next/static/media/miden_client_web.8318b253.wasm",
-        // 0.13.3
-        destination:
-          "https://miden-playground-assets.walnut.dev/_next/static/media/miden_client_web.24b3664b.wasm",
-      },
-      {
-        source: "/_next/static/media/miden_client_web.e6753bbd.wasm",
-        // 0.13.2
-        destination:
-          "https://miden-playground-assets.walnut.dev/_next/static/media/miden_client_web.e00a052b.wasm",
-      },
       {
         source: "/:path*",
         destination: `https://miden-playground.walnuthq.workers.dev/:path*`,
