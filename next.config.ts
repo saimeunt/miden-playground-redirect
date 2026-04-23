e140f5efae7ccede81c303af730e6cd5eb2f413d
saimeunt <saimeunt@gmail.com>
2026-04-23 08:54:45 +0200

    remove wasm redirects

--- next.config.ts
+++ next.config.ts
@@ -1,22 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  redirects: async () => [
-    {
-      source: "/_next/static/media/miden_client_web.8318b253.wasm",
-      // 0.13.3
-      destination:
-        "https://miden-playground-assets.walnut.dev/_next/static/media/miden_client_web.24b3664b.wasm",
-      permanent: true,
-    },
-    {
-      source: "/_next/static/media/miden_client_web.e6753bbd.wasm",
-      // 0.13.2
-      destination:
-        "https://miden-playground-assets.walnut.dev/_next/static/media/miden_client_web.e00a052b.wasm",
-      permanent: true,
-    },
-  ],
   rewrites: async () => ({
     beforeFiles: [
       {
